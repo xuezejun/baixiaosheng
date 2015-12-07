c160347c3fed6ee3e0c3004c42ccb7a156a7ac06
xuezejun <sxnwcxd@163.com>
2015-12-07 16:05:08 +0800

    更新

--- baixiaosheng/HomeTableCell.m
+++ baixiaosheng/HomeTableCell.m
@@ -16,30 +16,17 @@
     // Drawing code
 }
 */
+
+
 -(void)setTitleText:(NSString *)titleText;
 {
     NSLog(@"title~~%@",titleText);
     self.titleLable.text=titleText;
-
-//    [self.titleLable sizeToFit];
-//    DOFavoriteButton *dof=[[DOFavoriteButton alloc]initWithFrame:self.imageBtu.frame image:self.imageBtu.imageView.image imageFrame:self.imageBtu.frame];
-//    dof.imageColorOn =[UIColor colorWithRed:254/255 green:110/255 blue:111/255 alpha:1];
-//    dof.circleColor = [UIColor colorWithRed:254/255 green:110/255 blue:111/255 alpha:1];
-//    dof.lineColor = [UIColor colorWithRed:226/255 green:96/255 blue:96/255 alpha:1];
-//    [self addSubview:dof];
-    
     CGRect frome=self.imageBtu.frame;
     frome.origin.x=(WIDTH(self)-[self boundingRectWithSize:self.titleLable.frame.size WithFont:self.titleLable.font WithText:titleText].width-22)/2;
-//    frome.origin.x=(WIDTH(self)-25-WIDTH(self.titleLable))/2;
-    
     self.imageBtu.frame=frome;
-//    NSLog(@"%f~~~%f~~~````%f",[self boundingRectWithSize:self.titleLable.frame.size WithFont:self.titleLable.font WithText:titleText].width,WIDTH(self),frome.origin.x);
-
-    
-    
     CGRect tfrome=self.titleLable.frame;
     tfrome.origin.x=frome.origin.x+22;
-//    self.titleLable.text=titleText;
     tfrome.size.width=[self boundingRectWithSize:self.titleLable.frame.size WithFont:self.titleLable.font WithText:titleText].width;
     self.titleLable.frame=tfrome;
     self.lineLable.frame=CGRectMake(WIDTH(self),Y(self.lineLable),WIDTH(self.lineLable),HEIGHT(self.lineLable));
